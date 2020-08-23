f059a3aa91c763c544840ae866733f3a569b2ef7
Robert <robert.prus87@gmail.com>
2020-08-23 18:28:22 +0200

    Reformates codes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
-import React from 'react';
-import Header from './Header';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import RecomendedVideos from "./RecomendedVideos";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      {/* <h1>GoOOod MoOorning, HellSpawn287! 
-        <span role="img"> 😎  </span>
-      </h1>
-      <h2>Let's build YouTube Clone 
-      <span role="img"> 🚀 </span>
-      </h2> */}
-
-      {/* HEAder */}
       <Header />
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-
-      {/* Sidebar */}
-      {/* Recomended Video */}
-
-
+      <div className="app_page">
+        <Sidebar />
+        <RecomendedVideos />
+      </div>
     </div>
   );
 }
